bcb7759e7aad583a791207682af6f4a77ccc7ce5
Ilê Caian <ile.caian@gmail.com>
2018-04-28 10:21:42 -0300

    002: Att fibonacci fast code

--- 002/fib_fast.cpp
+++ 002/fib_fast.cpp
@@ -8,16 +8,15 @@ typedef unsigned long long ULL;
 LD phi = (sqrt(5) + 1) / 2;
 LD phi_inverse = phi - 1;;
 
-LD fib(int i) {
-    return (pow(phi, i) - pow(-phi_inverse, i)) / sqrt(5);
+ULL fib(int i) {
+    return (ULL) ((pow(phi, i) - pow(-phi_inverse, i)) / sqrt(5));
 }
 
 
 int main(int argc, char** argv) {
-    LD i = strtold(argv[1], NULL);
+    int i = strtold(argv[1], NULL);
 
-    LD result = fib(i);
-    ULL rounded_result = (ULL) result;
-    printf("The result is %llu\n", rounded_result);
+    ULL result = fib(i);
+    printf("The result is %llu\n", result);
     return 0;
 }
