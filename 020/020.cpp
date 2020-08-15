584314b06b3b8b482920d5ff86198ec6f1da0d6b
Ilê Caian <ile.caian@gmail.com>
2020-08-15 19:41:03 -0300

    feat(020): solve

---
+++ 020/020.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+class BigNum {
+    private:
+        vector<int> * _value;
+
+    public:
+        BigNum(int value) {
+            this->_value = new vector<int>();
+            int actual = value;
+
+            if (actual == 0) {
+                this->_value->push_back(actual);
+            } else {
+                while (actual > 0) {
+                    this->_value->push_back(actual % 10);
+                    actual = actual / 10;
+                }
+            }
+        }
+
+        BigNum(vector<int> * value): _value(value) {}
+
+        BigNum * sum(BigNum * with) {
+            vector<int> * result = new vector<int>();
+            int carry = 0;
+            int fullSize = this->size() > with->size() ? this->size() : with->size();
+
+            for (int i = 0; i < fullSize; i++) {
+                int actual = this->get_single_value(i) + with->get_single_value(i) + carry;
+
+                carry = 0;
+                if (actual > 9) {
+                    actual = actual - 10;
+                    carry = 1;
+                }
+
+                result->push_back(actual);
+            }
+
+            if (carry) {
+                result->push_back(carry);
+            }
+
+            return new BigNum(result);
+        }
+
+        BigNum * multiply(BigNum * by) {
+            BigNum * final = new BigNum(0);
+            vector<int> * result = new vector<int>();
+
+            int carry = 0;
+
+            for (int i = 0; i < this->size(); i++) {
+                int iActual = this->get_single_value(i);
+
+                for(int w = 0; w < i; w++) {
+                    result->push_back(0);
+                }
+
+                for (int j = 0; j < by->size(); j++) {
+                    int jActual = by->get_single_value(j);
+
+                    int actualResult = (iActual * jActual) + carry;
+
+                    carry = 0;
+                    if (actualResult > 9) {
+                        carry = actualResult / 10;
+                        actualResult = actualResult % 10;
+                    }
+
+                    result->push_back(actualResult);
+                }
+
+                if (carry) {
+                    result->push_back(carry);
+                    carry = 0;
+                }
+
+                BigNum * actualBigNum = new BigNum(result);
+                final = final->sum(actualBigNum);
+                result->clear();
+            }
+
+            return final;
+
+        }
+
+        int size() {
+            return this->_value->size();
+        }
+
+        int get_sum_of_digits() {
+            int rv = 0;
+            for (int i = 0; i < this->_value->size(); i++) {
+                rv += this->_value->at(i);
+            }
+
+            return rv;
+        }
+
+        string get_value() {
+            string result;
+            for (int i = 0; i < this->_value->size(); i++) {
+                result = to_string(this->_value->at(i)) + result;
+            }
+
+            return result;
+        }
+
+    private:
+        int get_single_value(int index) {
+            if (index >= this->_value->size()) {
+                return 0;
+            }
+
+            return this->_value->at(index);
+        }
+};
+
+int solve() {
+    BigNum * result = new BigNum(1);
+    BigNum * actual;
+
+    for (int i = 1; i <= 100; i++) {
+        actual = new BigNum(i);
+
+        result = actual->multiply(result);
+
+        delete actual;
+    }
+
+    int final = result->get_sum_of_digits();
+
+    delete result;
+
+    return final;
+}
+
+int main(int argc, char** argv) {
+    int rv = solve();
+    cout << "result = " << rv << endl;
+}
---
+++ 020/readme.md
@@ -0,0 +1,12 @@
+# 020 - Factorial Digit Sum
+
+`n!` means `n × (n − 1) × ... × 3 × 2 × 1`
+
+For example, `10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800`,
+and the sum of the digits in the number `10!` is `3 + 6 + 2 + 8 + 8 + 0 + 0 = 27`.
+
+Find the sum of the digits in the number `100!`
+
+## Disclaimer
+
+I largelly encourage you try to **solve by yourself** on the Project Euler site and by your own research. If you want to know **how** my solution works, go to `solution.md` file.
